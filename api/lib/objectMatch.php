7882c4e05522f95c1ef7cebdf740ab77322d7e25
webzuu <info@webzuu.pl>
2015-06-14 03:11:38 -0500

    object match debug mode + logic fixes

--- api/lib/objectMatch.php
+++ api/lib/objectMatch.php
@@ -7,7 +7,8 @@
 		"TAGS" powinien by� JSON-owym STRINGIEM odpowiadaj�cym temu, co dany obiekt chce i oferuje
 		*/
 		
-		private function point_match ($chce, $oferuje) {		    		    
+		private function point_match ($chce, $oferuje) {
+		    $this->debugMsg('point_match');		    		    
 			//geograficzne
 			if (isset($chce -> lat)) {
 				//echo PHP_EOL.$chce->address.' > '.$oferuje->address.PHP_EOL;
@@ -16,7 +17,7 @@
 				//echo 'match '.$chce->lat.','.$chce->lng.','.$chce->radius.' <> '.$oferuje->lat.','.$oferuje->lng.','.$oferuje->radius.PHP_EOL;
 				if (distance(Array($chce->lat,$chce->lng), Array($oferuje->lat,$oferuje->lng)) 
 				<= floatval($chce ->radius)+floatval($oferuje ->radius)) {
-					//echo 'MATCHED';
+					$this->debugMsg('Localization matched '.$chce->address.' with '.$oferuje->address);
 					return true;
 				} else {
 					return false;
@@ -32,9 +33,11 @@
 		}
 		
 		private function main_match ($chce, $oferuje) {
-			//return true;	
+			$this->debugMsg('main_match');
 			if ((bool)$chce -> all === true) {
+			    $this->debugMsg('main_match chce->all');
 				if ((bool)$chce -> block === true) {
+				    $this->debugMsg('main_match chce->block');
 					$n = count ($chce -> points);
 					$i = 0;
 					foreach ($oferuje->points as $pkt2) {
@@ -91,21 +94,33 @@
 		}
 		
 		private function chce_oferuje ($chce, $oferuje) {
-			if ($chce == NULL || $oferuje == NULL) { return false; }
+		    if($oferuje == NULL || $chce == NULL) {
+		        return false;
+		    }			
 			foreach ($chce as $nazwa=>$wartosc) {
 				//brak odpowiedniego pola w oferowanym
-				if (!isset ($oferuje->$nazwa)) {
+				if (!isset ($oferuje->$nazwa)) {				    
+				    $this->debugMsg('Brak pola w oferowanym ('.$nazwa.')');
 					return false;
 				}
-				if (objectMatch::main_match ($wartosc, $oferuje->$nazwa) == false) {
-					//echo 'false main_match';
+				if ($this->main_match ($wartosc, $oferuje->$nazwa) == false) {
+					$this->debugMsg('False w main_match '.$nazwa);
 					return false;
 				}
 			}
 			return true;
 		}
+        
+        private function debugMsg($msg) {
+            if($this->_debug) {
+                echo $msg.PHP_EOL;
+            }
+        }
 			
 		public function match ($o1, $o2) {
+		    
+            $this->_debug = false;
+            
 			//znajdujemy rodzic�w
 			$p1 = $o1;
 			while ($p1['PARENT_ID']>0) {
@@ -119,8 +134,16 @@
 			}
 		
 			if ($p1['ID']==$p2['ID']) return false;
+            
+            
+            if($o1['ID']==$_GET['test-match-with']) { $debugMode = true; }
 			
-			//echo $o1['NAME'].' vs '.$o2['NAME'].PHP_EOL;
+            if($debugMode) {
+             $this->_debug = true;
+			 echo $o1['ID'].':'.$o1['NAME'].' vs '.$o2['ID'].':'.$o2['NAME'].PHP_EOL.PHP_EOL;
+                $o1info = $o1;
+                $o2info = $o2;
+            }
 			
 			$o1 = json_decode ($o1['TAGS']);
 			$o2 = json_decode ($o2['TAGS']);      
@@ -128,18 +151,39 @@
 			if (($o1 === false) || ($o2 === false)) {
 				return false;
 			}
+            
+            $getTemplateId = function($objTags) {                
+                return $objTags->_meta->_template_id->points[0]->value;
+            };
+            
+            $templateMatched = ($getTemplateId($o1) == $getTemplateId($o2));
 			
-			$result1 = $this->chce_oferuje ($o1->wants, $o2->offers);
-			$result2 = $this->chce_oferuje ($o2->wants, $o1->offers);
-			$result = ($result1 && $result2);
+            if($templateMatched) {                 
+                 
+                $result1 = $this->chce_oferuje ($o2->wants, $o1->offers);                 
+		        $result2 = $this->chce_oferuje ($o1->offers, $o2->wants);
+                
+                if(!$o1->wants) { $result1 = $result2 = $this->chce_oferuje($o2->wants,$o1->offers); }
+                if(!$o2->wants) { $result1 = $result2 = $this->chce_oferuje($o1->wants,$o2->offers); }
+                 
+            }
+			$result = ($templateMatched && $result1 && $result2);
 			
-			//var_dump($result1);
-			//var_dump($result2);
-			//var_dump($result);
+            if($debugMode) {
+             if(!$templateMatched) {
+                 echo 'NOT MATCHED: Different template'.PHP_EOL;
+             } else {
+                 echo 'MATCHED: Same template'.PHP_EOL;
+             }             
+             echo $o1info['NAME'].' tags:'.PHP_EOL;
+             echo json_encode($o1).PHP_EOL.PHP_EOL;
+             echo $o2info['NAME'].' tags:'.PHP_EOL;
+             echo json_encode($o2).PHP_EOL.PHP_EOL;
+			 echo 'chce_oferuje('.$o1info['ID'].'->offers,'.$o2info['ID'].'->wants): '.((int)$result1).PHP_EOL;
+             echo 'chce_oferuje('.$o2info['ID'].'->wants,'.$o1info['ID'].'->offers): '.((int)$result2).PHP_EOL;
+             echo PHP_EOL;
+            }
 			
 			return $result;
 		}
 	}
-
-?>
-	
